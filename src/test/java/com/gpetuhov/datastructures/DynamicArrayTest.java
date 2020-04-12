96c1f9104d7a73c75dba8ef8d2869fafe479b264
Gennady Petukhov <g3pmail@gmail.com>
2020-04-12 19:11:39 +0300

    Add tests

---
+++ src/test/java/com/gpetuhov/datastructures/DynamicArrayTest.java
@@ -0,0 +1,13 @@
+package com.gpetuhov.datastructures;
+
+import org.junit.Test;
+
+public class DynamicArrayTest {
+
+    private DynamicArray array = new DynamicArray();
+
+    @Test
+    public void hello() {
+        array.hello();
+    }
+}
